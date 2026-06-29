25087b7e25bf7f1f09e8e851acdf4e3dce8e3fa6
NP1R777 <zaharovila780@gmail.com>
2026-06-29 21:15:38 +0700

    Refactor any files

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
-from datetime import timedelta
-from decouple import config
 import os
+from pathlib import Path
+from decouple import config
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- backend/app/app/wsgi.py
+++ backend/app/app/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(BASE_DIR, 'my_app1'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.app.settings')
 
 application = get_wsgi_application()
